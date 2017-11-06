b22dd7101291e1d7e92160e094a35f75a56411a6
Daniel Kafer <daniel.j.kafer@gmail.com>
2017-11-06 19:04:17 +0000

    added login, add review and remove review routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import express, { router } from 'express';
 
 import adminController from './controllers/admin';
+import authController from './controllers/auth';
 
-router.get('/', )
+router.get('/login', adminController.logIn);
+
+router.post('/review', authController.verifyToken, adminController.addReview);
+
+router.delete('/review', authController.verifyToken, adminController.removeReview);
 
 export default router;
