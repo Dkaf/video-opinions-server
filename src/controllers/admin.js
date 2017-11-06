e0492598c533b220f03cd0155d34d399b1790b53
Daniel Kafer <daniel.j.kafer@gmail.com>
2017-11-06 19:08:40 +0000

    added login and remove review

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,3 +1,4 @@
+import app from './../app';
 import Review from './../models/review';
 import User from './../models/user';
 import jwt from 'jsonwebtoken';
@@ -5,11 +6,15 @@ import bcrypt from 'bcrypt-nodejs';
 
 const adminController = {};
 
+//POST Login
 adminController.logIn = (req, res) => {
     const  { password } = req.body;
     User.findOne({'name': 'admin'})
     .then( user => {
-        if (password = user.password) {
+        let payload = {
+            id: user._id
+        };
+        if (password == user.password) {
             let token = jwt.sign(payload, app.get('secret'), {expiresIn: '2h'});
             res.status(201).json({
                 success: true,
@@ -25,6 +30,7 @@ adminController.logIn = (req, res) => {
     })
 }
 
+//POST Add Review
 adminController.addReview = (req, res) => {
     const { title, thumbnail, review, type } = req.body;
     let newReview = new Review({
@@ -37,6 +43,25 @@ adminController.addReview = (req, res) => {
 
     newReview.save();
 
+}
+
+//DELETE Remove Review
+adminController.removeReview = (req, res) => {
+    const { id } = req.body;
+    Review.findOneAndRemove({'_id': id})
+    .then( data => {
+        res.status(200).json({
+            success: true,
+            message: 'review removed',
+            data: data
+        });
+    })
+    .catch( err => {
+        res.status(500).json({
+            success: false,
+            err: err
+        })
+    })
 }
 
 export default adminController;
