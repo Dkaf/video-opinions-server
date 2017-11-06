0a5cf67f93e300724eaf8c345ddc69f9862d61c0
Daniel Kafer <daniel.j.kafer@gmail.com>
2017-11-06 19:09:59 +0000

    added auth controller

---
+++ src/controllers/auth.js
@@ -0,0 +1,29 @@
+import app from './app';
+import jwt from 'jsonwebtoken';
+
+const authController = {};
+
+
+
+authController.verifyToken = (req, res, next) => {
+	let token = req.body.token || req.query.token || req.headers['x-access-token'];
+    
+	if(token) {
+		jwt.verify(token, app.get('secret'), (err, decoded) => {
+			if(err) {
+				res.json({success: false, message: 'Failed to verify token'});
+				throw err;
+			} else if(!decoded.id || !decoded.username) {
+				return res.json({success: false, message: 'No Payload in token'});
+			}
+			else {
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+		res.status(403).json({success: false, message: 'no token provided'});
+	}
+};
+
+export default authController;
