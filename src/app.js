307695099d8d553b4423f93b152dce585fd36778
Daniel Kafer <daniel.j.kafer@gmail.com>
2017-11-07 17:07:49 +0000

    commented out database connection temporarily

--- src/app.js
+++ src/app.js
@@ -3,18 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
-import routes from './routes';
-import config from 'config';
-import dotenv from 'dotenv';
-dotenv.config();
+import router from './routes';
 
-let DBHost = config.get('DBHost');
+// let DBHost = config.get('DBHost');
 
-mongoose.connect(DBHost, {
-	useMongoClient: true
-}).then(console.log("Connected to mongodb..."));
-let db = mongoose.connection;
-db.on('error', console.error.bind('connection error'));
+// mongoose.connect(DBHost, {
+// 	useMongoClient: true
+// }).then(console.log("Connected to mongodb..."));
+// let db = mongoose.connection;
+// db.on('error', console.error.bind('connection error'));
 
 const app = express();
 
@@ -23,6 +20,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.set('secret', process.env.SECRET_KEY);
 
-app.use('/admin', routes);
+app.use('/admin', router);
 
 export default app;
