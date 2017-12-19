ba004a552b90ee2e3286023903c88ab13e6cfeb9
Unknown <Daniel.j.kafer@gmail.com>
2017-12-19 12:49:23 -0600

    Added new routes
    added route for getReviews and changePassword

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,18 @@ import express from 'express';
 
 import adminController from './controllers/admin';
 import authController from './controllers/auth';
+import reviewController from './controllers/review';
 
 const router = express();
+
+router.get('/review', reviewController.getReviews);
 
 router.post('/login', adminController.logIn);
 
 router.post('/review', authController.verifyToken, adminController.addReview);
 
 router.delete('/review', authController.verifyToken, adminController.removeReview);
+
+router.post('/settings', authController.verifyToken, adminController.changePassword);
 
 export default router;
